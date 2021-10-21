c160ad4431f97abfb5c927bdc4b423284c5516a0
Mahesh Gaikwad <91421427+777ahesh@users.noreply.github.com>
2021-10-21 15:45:34 +0530

    Cards

---
+++ ReactJs Basics/Sdata.jsx
@@ -0,0 +1,25 @@
+const Sdata = [
+    {
+    id : 1, 
+    sname: "DARK",
+    imgsrc: "https://wallpapercave.com/w/wp4056410.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80100172",
+    },
+    {
+    id : 2,
+    sname: "Gunjan Saxena",
+    imgsrc: "https://wallpapercave.com/wp/wp7305941.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/81292944",
+    },
+    {
+    id: 3,
+    sname: "Dangal",
+    imgsrc: "https://wallpapercave.com/wp/wp4627605.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80166185",
+    }
+]
+
+export default Sdata;
